8063a79735db5c3f8490f89d2b1f144dee327520
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2024-02-24 20:16:31 -0800

    added routes for profile and about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     
 </script>
-<h1>Welcome to Meowbits</h1>
+<h1>THIS IS OUR ABOUT PAGE</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <style>
   
