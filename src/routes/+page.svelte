01d0a5bbbc76fb5963111a20a6d04bdab70af84c
marsxnow <raxelportiz@gmail.com>
2024-02-24 21:23:57 -0800

    Update navbar and page styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-    
+    import Navbar from "../components/navbar.svelte";
 </script>
+<Navbar />
+<body>
 <h1>Welcome to Meowbits</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</body>
+<style>
+    h1 {
+        font-family: 'Space Font', monospace;
+        color: #ffffff;
+    }
+    p {
+        font-family: 'Space Font', monospace;
+        color: #ffffff;
+    }
+    body {
+        background-color: #282323;
+    }
+</style>
