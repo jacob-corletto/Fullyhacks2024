d0ade6e1aa55b2218a5a1e986275a3c5d9751387
esteban-zapata <ezapata2002@gmail.com>
2024-02-24 23:35:33 -0800

    lol

---
+++ src/components/about.svelte
@@ -0,0 +1,16 @@
+<section class="about">
+    <h2>Meet the Team!</h2>
+  </section>
+  
+  <section class="team">
+    <ul>
+      <li>
+        <h3>Raxel Ortiz</h3>
+        <h3>Nick Chung</h3>
+        <h3>Jacob Corletto</h3>
+        <h3>Esteban Zapata</h3>
+      </li>
+      </ul>
+      
+  </section>
+  
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,40 @@
-<script>
-    
-</script>
-<h1>THIS IS OUR ABOUT PAGE</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<style>
+<section class="about about-dark">
+    <h2>Meet the Team!</h2>
+  </section>
   
-</style>
+  <section class="team team-dark">
+    <ul>
+      <li class="team-member team-dark-member">
+        <h3>Raxel Ortiz</h3>
+      </li>
+      <li class="team-member team-dark-member">
+        <h3>Nick Chung</h3>
+      </li>
+      <li class="team-member team-dark-member">
+        <h3>Jacob Corletto</h3>
+      </li>
+      <li class="team-member team-dark-member">
+        <h3>Esteban Zapata</h3>
+      </li>
+    </ul>
+  </section>
+
+  
+  
+  <style>
+.team-dark {
+  background-color: black;
+  color: white;
+}
+
+.team-dark-member {
+  box-shadow: 0 0 5px #FF69B4, 0 0 10px #FF69B4, 0 0 20px #FF69B4;
+}
+    .team-member {
+      box-shadow: 0 0 5px #FF69B4, 
+      0 0 10px #FF69B4, 
+      0 0 20px #FF69B4;
+    }
+  </style>
+
+  
